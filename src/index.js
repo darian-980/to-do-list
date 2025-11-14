b32fa5c9f487ced64a5f803620d0d5f0a4822248
Devolas <darian.dihal@gmail.com>
2025-11-14 17:47:04 -0500

    working project selector, w css

---
+++ src/index.js
@@ -0,0 +1,103 @@
+import {constructProjectList} from "./projectListDom.js";
+
+
+class todoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = "active";
+        this.id = crypto.randomUUID();
+    }
+
+    info() {
+        return ("[" + this.title + "] [" + this.description + "] [" + this.dueDate + "] [" + this.priority + "] [" + this.status + "] [" + this.id + "]");
+    }
+}
+
+class projectItem {
+    constructor(title) {
+        this.title = title;
+        this.itemList = [];
+        this.id = crypto.randomUUID();
+    }
+
+    appendItem(task) {
+        this.itemList.push(task);
+    }
+
+    removeItem(id) {
+        const index = this.itemList.findIndex(item => item.id === id); //find array item with matching id
+        const itemName = this.itemList[index].title; //grab name of task to be removed (for documentation only)
+        const removeTask = this.itemList.splice(index, 1); //remove 1 item at specified index
+        console.log("removed '" + itemName + "' task from '" + this.title + "' project");
+    }
+
+    listInfo() {
+        return (this.itemList.map(item => item.title)); //uses .map to grab the title value from every task item
+    }
+}
+
+class projectList {
+    constructor() {
+        this.projectList = [];
+        this.id = crypto.randomUUID(); // probably not needed
+    }
+
+    appendProject(project) {
+        this.projectList.push(project);
+    }
+
+    removeProject(id) {
+        const index = this.projectList.findIndex(project => project.id === id); //find array item with matching id
+        const removeProject = this.projectList.splice(index, 1); //remove 1 item at specified index
+    }
+}
+
+const mainProjectList = new projectList(); // the one and only project list to be used globally
+
+(function () {
+    const project2 = new projectItem("Daytime");
+    const item1 = new todoItem("floss", "floss teeth", "tonight", "medium");
+    const item2 = new todoItem("shower", "wash hair", "tonight", "high");
+    project2.appendItem(item1);
+    project2.appendItem(item2);
+
+
+
+    const project3 = new projectItem("Evening");
+    const item3 = new todoItem("contacts", "swap contacts for new ones", "morning", "low");
+    project3.appendItem(item3);
+
+
+
+    console.log("created " + project2.title)
+    mainProjectList.appendProject(project2);
+    mainProjectList.appendProject(project3);
+    constructProjectList(mainProjectList, selectProject);
+})();
+
+
+function selectProject() { //do something when an element is clicked
+
+};
+
+// export default mainProjectList;
+
+// const item1 = new todoItem("floss", "floss teeth", "tonight", "medium");
+// const item2 = new todoItem("shower", "wash hair", "tonight", "high");
+// const item3 = new todoItem("contacts", "swap contacts for new ones", "morning", "low");
+// console.log(item1.info());
+// console.log(item2.info());
+// console.log(item3.info());
+
+// const project1 = new projectItem("nighttime");
+// project1.appendItem(item1);
+// project1.appendItem(item2);
+// project1.appendItem(item3);
+// console.log(project1.listInfo());
+// project1.removeItem(item2.id);
+// console.log(project1.listInfo());
+
+
---
+++ src/projectListDom.js
@@ -0,0 +1,33 @@
+export function constructProjectList(mainProjectList, onProjectClick) {
+
+     const existingList = document.getElementById(`dynamic-project-list`); //grab dynamic project list if it already exists
+
+     existingList?.remove(); // delete dynamic project list if it already exists
+
+
+     const projectListDiv = document.getElementById(`project-list`);
+
+     const projectListDynamic = document.createElement("div"); // make a new element that we can delete at any time to recreate/refresh a new list
+     projectListDynamic.setAttribute("id", "dynamic-project-list");
+     projectListDiv.appendChild(projectListDynamic);
+
+     for (const project of mainProjectList.projectList) {
+          console.log(project)
+          const projectItem = document.createElement("div");
+          projectItem.setAttribute("id", project.id); // add the id to the element so that it can be referenced
+          projectItem.textContent = project.title; // set project title
+          projectItem.addEventListener('click', () => { 
+               onProjectClick(project);
+               activeProjectSelect(projectItem);
+               console.log("item clicked");
+               }
+          );
+
+          projectListDynamic.appendChild(projectItem); // add project to the dynamic list
+     }
+}
+
+function activeProjectSelect(projectItem) {
+     document.querySelectorAll('#dynamic-project-list > div').forEach(div => div.classList.remove('activeProject')); //remove "activeProject" class from all projects
+     projectItem.setAttribute("class", "activeProject"); // add "activeProject" class to provided project
+};
