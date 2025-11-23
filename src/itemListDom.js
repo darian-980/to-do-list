9fe02c54d704c638890619b641d46858e7dd45b1
Devolas <darian.dihal@gmail.com>
2025-11-22 23:38:34 -0500

    clicking on an item displays details and is highlighted

--- src/itemListDom.js
+++ src/itemListDom.js
@@ -16,18 +16,37 @@ export function constructItemList(projectItem) {
           const item_task = document.createElement("div");
           item_task.setAttribute("id", item.id); // add the id to the element so that it can be referenced
           item_task.textContent = item.title; // set project title
-          item_task.addEventListener('click', () => { 
+          item_task.addEventListener('click', () => {
                // onProjectClick(item);
-               // activeProjectSelect(projectItem);
+               activeItemSelect(item, item_task);
                console.log("item clicked");
-               }
+          }
           );
 
           itemListDynamic.appendChild(item_task); // add project to the dynamic list
      }
+
+     function activeItemSelect(Item, item_task) {
+          document.querySelectorAll('#dynamic-item-list > div').forEach(div => div.classList.remove('activeItem')); //remove "activeProject" class from all projects
+          item_task.setAttribute("class", "activeItem"); // add "activeItem" class to provided project
+
+          document.querySelectorAll('#itemExpand').forEach(div => div.remove()); //remove all itemExpands
+          const itemExpand = document.createElement("div");
+          itemExpand.setAttribute("id", "itemExpand");
+
+          const itemDescription = document.createElement("div");
+          itemDescription.textContent = Item.description;
+          itemDescription.setAttribute("class", "itemDescription");
+          itemExpand.appendChild(itemDescription);
+
+          const itemdueDate = document.createElement("div");
+          itemdueDate.textContent = Item.dueDate;
+          itemdueDate.setAttribute("class", "itemDueDate");
+          itemExpand.appendChild(itemdueDate);
+
+          item_task.appendChild(itemExpand);
+
+
+     };
 }
 
-function activeProjectSelect(projectItem) {
-     document.querySelectorAll('#dynamic-project-list > div').forEach(div => div.classList.remove('activeProject')); //remove "activeProject" class from all projects
-     projectItem.setAttribute("class", "activeProject"); // add "activeProject" class to provided project
-};
